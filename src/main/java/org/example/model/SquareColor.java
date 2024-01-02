ae4cfe1d7bfe6dab9739e88fde49869fd5cd23e5
keeeper85 <122378010+keeeper85@users.noreply.github.com>
2024-01-02 20:07:24 +0800

    Enum Colors have Color values.

--- src/main/java/org/example/model/SquareColor.java
+++ src/main/java/org/example/model/SquareColor.java
@@ -4,21 +4,30 @@ import java.awt.*;
 
 public enum SquareColor {
 
-    YELLOW,
-    GOLD,
-    BEIGE,
-    ORANGE,
-    PINK,
-    MAGENTA,
-    RED,
-    CRIMSON,
-    CANARY,
-    GREEN,
-    TEAL,
-    BLUE,
-    PURPLE,
-    TAN,
-    BROWN,
-    AQUA
+    YELLOW (new Color(255, 255, 0)),
+    GOLD (new Color(255, 180, 0)),
+    BEIGE (new Color(245, 225, 220)),
+    ORANGE (new Color(255, 100, 0)),
+    PINK (new Color(255, 100, 180)),
+    MAGENTA (new Color(255, 0, 255)),
+    RED (new Color(255, 0, 0)),
+    CRIMSON (new Color(150, 0, 0)),
+    CANARY (new Color(150, 255, 0)),
+    GREEN (new Color(0, 100, 0)),
+    TEAL (new Color(0, 120, 120)),
+    BLUE (new Color(0, 100, 255)),
+    PURPLE (new Color(120, 0, 120)),
+    TAN (new Color(220, 170, 100)),
+    BROWN (new Color(150, 75, 0)),
+    AQUA (new Color(0, 255, 255));
 
+    private final Color color;
+
+    SquareColor(Color color) {
+        this.color = color;
+    }
+
+    public Color getColor() {
+        return color;
+    }
 }
