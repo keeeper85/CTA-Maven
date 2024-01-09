46ecc6bc00f289d45a8cd6106839a88c46ea6549
keeeper85 <122378010+keeeper85@users.noreply.github.com>
2024-01-09 14:30:56 +0800

    Tests for square creation are complete.

---
+++ src/test/java/org/example/model/SquareFactoryTest.java
@@ -0,0 +1,34 @@
+package org.example.model;
+
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class SquareFactoryTest {
+
+    static List<Square> list;
+
+    @BeforeAll
+    static void createYellowSquares(){
+        list = SquareFactory.createYellowSquares(100);
+    }
+
+    @Test
+    void testAmountCreated(){
+        int result = list.size();
+        assertEquals(100,result);
+    }
+
+    @Test
+    void testSquareNames(){
+        boolean testFailed = false;
+        for (Square square : list) {
+            String squareName = square.name;
+            if (!squareName.contains("yellow")) testFailed = true;
+        }
+        assertEquals(false, testFailed);
+    }
+}
---
+++ src/test/java/org/example/model/strategy/EasyStrategyTest.java
@@ -0,0 +1,82 @@
+package org.example.model.strategy;
+
+import org.example.model.Square;
+import org.example.model.constants.Constants;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class EasyStrategyTest {
+
+    static List<Square> allSquaresCreated;
+    @BeforeAll
+    static void createSquares() {
+        EasyStrategy easy = new EasyStrategy();
+        allSquaresCreated = easy.getSquares();
+    }
+
+    @Test
+    void testAmountCreated(){
+        int result = allSquaresCreated.size();
+        int expected = Constants.TOTAL_SQUARES_AMOUNT;
+        assertEquals(expected,result);
+    }
+
+    @Test
+    void isNumberOfYellowSquaresDividableByThree(){
+        int count = 0;
+        for (Square square : allSquaresCreated) {
+            if (square.name.contains("yellow")) count++;
+        }
+        assertEquals(0, count % 3);
+    }
+
+    @Test
+    void isNumberOfGreenSquaresDividableByThree(){
+        int count = 0;
+        for (Square square : allSquaresCreated) {
+            if (square.name.contains("green")) count++;
+        }
+        assertEquals(0, count % 3);
+    }
+
+    @Test
+    void isNumberOfPinkSquaresDividableByThree(){
+        int count = 0;
+        for (Square square : allSquaresCreated) {
+            if (square.name.contains("pink")) count++;
+        }
+        assertEquals(0, count % 3);
+    }
+
+    @Test
+    void isNumberOfCrimsonSquaresDividableByThree(){
+        int count = 0;
+        for (Square square : allSquaresCreated) {
+            if (square.name.contains("crimson")) count++;
+        }
+        assertEquals(0, count % 3);
+    }
+
+    @Test
+    void isNumberOfTealSquaresDividableByThree(){
+        int count = 0;
+        for (Square square : allSquaresCreated) {
+            if (square.name.contains("teal")) count++;
+        }
+        assertEquals(0, count % 3);
+    }
+
+    @Test
+    void isNumberOfGoldSquaresDividableByThree(){
+        int count = 0;
+        for (Square square : allSquaresCreated) {
+            if (square.name.contains("gold")) count++;
+        }
+        assertEquals(0, count % 3);
+    }
+
+}
